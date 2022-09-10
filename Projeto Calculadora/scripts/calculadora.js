328ffe7e242adcbf5e0b9c9497982e05d803f12a
Iverson Rômulo Gonçalves <iverson-14@hotmail.com>
2022-09-10 14:52:16 -0300

    ok

--- Projeto Calculadora/scripts/calculadora.js
+++ Projeto Calculadora/scripts/calculadora.js
@@ -1,16 +1,25 @@
 function ligar() {
     telacalc.style.background="white";
     telacalc.style.color="black";
+    avisold2.style.background="#008000b9";
+    avisold.style.background="#ffffff32";
     document.getElementById('textotela').innerHTML='';
+    document.getElementById('avisold2').innerHTML='ON';
+    document.getElementById('avisold').innerHTML='';
+
 }
 
 function desligar() {
     telacalc.style.background="black";
     telacalc.style.font="black";
+    avisold.style.background="#a90909ad";
+    avisold2.style.background="#ffffff32";
     document.getElementById('textotela').innerHTML='';
+    document.getElementById('avisold').innerHTML='OFF';
+    document.getElementById('avisold2').innerHTML='';
 }
 
-function insert(escrita){
+function caracter(escrita){
     let textotela = document.getElementById('textotela').innerHTML;
     document.getElementById('textotela').innerHTML = textotela + escrita;
 }
