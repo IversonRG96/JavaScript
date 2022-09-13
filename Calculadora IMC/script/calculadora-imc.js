c89d25bf3445517d698662dbd407028abcb2c22f
Iverson Rômulo Gonçalves <iverson-14@hotmail.com>
2022-09-13 16:07:36 -0300

    ok

--- Calculadora IMC/script/calculadora-imc.js
+++ Calculadora IMC/script/calculadora-imc.js
@@ -1,38 +1,39 @@
 function calcular(){
-    let usuario = document.getElementById('nome');
-    let peso = document.getElementById('peso').value;
-    let altura = document.getElementById('altura').value;
-    let resultado = document.getElementById('resultado');
-    let imc;
-    let status;
+    let usuario = document.getElementById('nome')
+    let peso = document.getElementById('peso').value
+    let altura = document.getElementById('altura').value
+    let resultado = document.getElementById('resultado')
+    let imc
 
-   imc = peso/(altura*altura);
+    imc = peso/(altura*altura)
 
-   if (usuario!='' && peso!='' && altura!=''){
+    if (usuario!='' && peso!='' && altura!=''){
 
-        if (imc>=18.5 && imc<=24.9){
-            resultado.innerHTML='';
-            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação no IMC, você está com o Peso Normal.`;
+        if(imc<=18.49){
+            resultado.innerHTML=``
+            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação do IMC, você está Abaixo do Peso.`
         }
-        if (condition) {
-            elseif
-        } else {
-            
-        } (imc>=25 && imc<=29.99){
-            resultado.innerHTML='';
-            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação no IMC, você está com o Sobrepeso.`;
+        else if (imc >= 18.5 && imc <= 24.9){
+            resultado.innerHTML=``
+            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação do IMC, você está com o Peso Normal.`
         }
-        elseif (imc>=30 && imc<=39.99){
-            resultado.innerHTML='';
-            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação no IMC, você está com Obesidade.`;
+        else if (imc >= 25 && imc <= 29.9) {
+            resultado.innerHTML=``
+            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação do IMC, você está com Sobrepeso.`
         }
-        else (imc>=40){
-            resultado.innerHTML='';
-            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação no IMC, você está com Obesidade Grave.`;
+        else if (imc >= 30 && imc <= 34.9){
+            resultado.innerHTML=``
+            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação do IMC, você está com Obesidade I.`
         }
-
+        else if (imc >= 35 && imc <= 39.9){
+            resultado.innerHTML=``
+            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação do IMC, você está com Obesidade II.`
+        }
+        else {
+            resultado.innerHTML=``
+            resultado.innerHTML = `${usuario.value}, o seu IMC é igual a ${imc.toFixed(2)}. Segundo a classificação do IMC, você está com Obesidade III.`
+        }
     } else {
         alert('Preencha todos os campos!')
-    }  
+    }
 }
-
