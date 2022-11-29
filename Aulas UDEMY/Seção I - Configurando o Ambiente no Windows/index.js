ce2de0f989a99835e7d9f82b860ac004eacacc0d
Iverson Rômulo Gonçalves <iverson-14@hotmail.com>
2022-11-29 11:30:00 -0300

    Update index.js

--- Aulas UDEMY/Seção I - Configurando o Ambiente no Windows/index.js
+++ Aulas UDEMY/Seção I - Configurando o Ambiente no Windows/index.js
@@ -1,1 +1,10 @@
-console.log(`Meu nome é "Iverson". Estou aprendendo JavaScript ás`, 10, `da manhã.`);
+const nome = "Luiz Otávio";
+const sobreNome = "Miranda";
+const idade = 30;
+const altura = 1.8;
+const peso = 84;
+let imc = peso/(altura*altura);
+let anoNascimento = 2022 - idade;
+
+console.log(`${nome} ${sobreNome} tem ${idade} anos, pesa ${peso} kg, tem ${altura} de altura, e seu IMC é de ${imc}`);
+console.log(`${nome} nasceu em ${anoNascimento}.`);
