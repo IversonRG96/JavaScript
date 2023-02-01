05cb0363978626ba5db16e4f2a58306c1814beea
Iverson Rômulo Gonçalves <iverson-14@hotmail.com>
2023-02-01 14:38:16 -0300

    aula de js udemy

---
+++ Aulas UDEMY/Seção II - JS Básico - Iniciantes/appscript.js
@@ -0,0 +1,5 @@
+const num1 = Number(prompt('Digite um Número'));
+const num2 = Number(prompt('Digite outro Número'));
+
+const resultado = num1 + num2;
+alert(`O resultado é igual á ${resultado} !`)
