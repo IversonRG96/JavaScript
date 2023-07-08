c69587e355d5f52d502725f267dee3705cb68828
Iverson Rômulo Gonçalves <iverson-14@hotmail.com>
2023-07-08 12:04:34 -0300

    exercidio udemy

--- Aulas UDEMY/Seção III - JS - Lógica de Programação/aula 1/javascript.js
+++ Aulas UDEMY/Seção III - JS - Lógica de Programação/aula 1/javascript.js
@@ -3,7 +3,23 @@ let peso = document.getElementById('peso');
 let altura = document.getElementById('altura');
 
 function calcularImc() {
-    alert(`Seu peso é ${peso.value} e sua altura ${altura.value}`);
+    let resultado = peso.value/(altura.value*altura.value);
+
+    if (peso.value == '' && peso.value == 0 || altura.value == '' && altura.value == 0) {
+        alert(`ERRO: Dados incompletos`);
+    } else if(resultado < 18.5){
+        alert(`Seu IMC é igual a ${resultado.toFixed(2)}, você está classificado como Abaixo do Peso!`);
+    } else if (resultado >= 18.5 && resultado <= 24.99){
+        alert(`Seu IMC é igual a ${resultado.toFixed(2)}, você está classificado como Peso Normal!`);
+    } else if (resultado >= 25 && resultado <=29.99){
+        alert(`Seu IMC é igual a ${resultado.toFixed(2)}, você está classificado como Sobrepeso!`);
+    } else if (resultado >= 30 && resultado <= 34.99) {
+        alert(`Seu IMC é igual a ${resultado.toFixed(2)}, você está classificado como Obesidade 1!`);
+    } else if (resultado >= 35 && resultado <= 39.99){
+        alert(`Seu IMC é igual a ${resultado.toFixed(2)}, você está classificado como Obesidade 2!`);
+    } else if (resultado > 40){
+        alert(`Seu IMC é igual a ${resultado.toFixed(2)}, você está classificado como Obesidade 3!`);
+    } else {}
 }
 
 calcular.addEventListener('click',calcularImc);
