1d630156cfcafca3d6143be1a5ed4e0585a1afe9
Iverson Rômulo Gonçalves <iverson-14@hotmail.com>
2023-10-04 10:51:33 -0300

    novas aulas udemy

---
+++ Aulas UDEMY/Seção III - JS - Lógica de Programação/Aula_47 - Switch case/switch_case.js
@@ -0,0 +1,19 @@
+/*
+
+Switch Case sintáxe:
+
+switch(x) {
+    case 0:
+        x = 10;
+        break;
+    case 1:
+        y = 5;
+        break;
+    case 3:
+        z = 0;
+        break;
+    default:
+        a = '';
+}
+
+*/
---
+++ Aulas UDEMY/Seção III - JS - Lógica de Programação/Aula_50 - atrib via deses - arrays/avd_arrays.js
@@ -0,0 +1,27 @@
+
+// Atribuição via desestruturação (Array):
+
+//const cores = ["azul", "preto", "amarelo", "vermelho", "branco", "rosa"];
+
+//const [carro, garrafa, blusa, ... rest] = cores;//salva os valores de cores nas variáveis
+
+//console.log(carro, garrafa, blusa)
+//console.log(resto);
+
+// Acessar os indices através da A. via D. (Array)
+
+const monstros = [["Verdehile", "Baleygr", "Sath"], ["Icaru", "Sigmarus", "Verad"], ["Veromus", "Kro", "Otohime"]];
+const [Fogo, Agua, Escuridao] = monstros;
+
+console.log(Fogo[0]);
+console.log(Fogo[1]);
+console.log(Fogo[2]);
+console.log(Agua[0]);
+console.log(Agua[1]);
+console.log(Agua[2]);
+console.log(Escuridao[0]);
+console.log(Escuridao[1]);
+console.log(Escuridao[2]);
+
+
+
